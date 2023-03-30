23a50207a7e98d9f9c471b14743bd87c58cfeaff
ArcaneAria <arcanearia@gmail.com>
2023-03-30 11:30:31 -0700

    Added minigame menu images.

--- 1-build.bat
+++ 1-build.bat
@@ -2,16 +2,16 @@
 set working_name=working
 
 echo Clearing out the old files and creating a clean workspace...
-del /s /q cd\%working_name%\* 1>nul
+rmdir /s /q cd\%working_name% 1>nul
 Xcopy /E /q cd\orig\ cd\%working_name%\ 1>nul
 echo:
 
 echo Converting all images to cels and inserting...
-python tools\BMPToCEL.py images\ cd\working\
+::python tools\BMPToCEL.py images\ cd\working\
 echo:
 
 echo Converting bmp sprite sheets to anims...
-::python tools\BMPDirectoryToAnim.py anims anims_pre_process cd\working
+python tools\BMPDirectoryToAnim.py anims anims_pre_process cd\working
 ::tools\BMPTo3DOAnim.exe 24 24 anims\SUB_TITLE01.bmp cd\working\jyanpai\AnimationData\sub_title\SUB_TITLE01.ANIM
 echo:
 
